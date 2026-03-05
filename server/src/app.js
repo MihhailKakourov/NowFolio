aaefa86d2677de840206bba848520fc710b5a3f4
MihhailKakourov <mihhail.kakourov@ivkhk.ee>
2026-03-05 10:45:22 +0200

    init commit for structure

---
+++ server/src/app.js
@@ -0,0 +1,20 @@
+const fastify = require('fastify')({ logger: true });
+require('dotenv').config();
+
+fastify.register(require('@fastify/cors'), { 
+  origin: true // В продакшене тут будет адрес фронтенда
+});
+
+fastify.get('/', async (request, reply) => {
+  return { hello: 'API is running!' }
+});
+
+const start = async () => {
+  try {
+    await fastify.listen({ port: process.env.PORT || 3000, host: '0.0.0.0' });
+  } catch (err) {
+    fastify.log.error(err);
+    process.exit(1);
+  }
+};
+start();
